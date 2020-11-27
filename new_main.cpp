5b7849c92eb06f74434168343749347983c8c307
Jaszczomb52 <cmdr.blackburn@gmail.com>
2020-11-27 22:19:18 +0100

    kod do plikow

--- new_main.cpp
+++ new_main.cpp
@@ -1,3 +1,4 @@
+#include "pch.h"
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -23,17 +24,23 @@ int main()
 
 //    std::cout << "Wprowadz nazwe pliku: " << std::endl;
 
+	std::cout << "Podaj sciezke pliku z ktorego ma zostac pobrana ilosc wierszy i kolumn do macierzy" << std::endl;
+
     std::cin >> plikA;
 
-    los.open("plikA.txt", std::ofstream::out | std::ofstream::app);
+	los.open(plikA, std::ios::in | std::ios::out);
+	if (los.good())
+	{
+		while (!plik.eof())
+		{
+			los >> n;
+			los >> m;
+		}
+	}
 
 //    void open ( const std::string& plikA, std::ios_base::openmode mode = std::ios_base::out);
 
-    std::cout << "Podaj ile turbo wierszy pokazac" << std::endl;
-
-    std::cin >> n >> m;
-
-    for (int i = 1; i <= n; ++i) {
+ /*   for (int i = 1; i <= n; ++i) {
         std::cout << i << ": " ;
         for (int j = 1; j <= m ; ++j) {
            re = random_dev();
@@ -41,7 +48,8 @@ int main()
            los.width(6);
            std::cout << re << std::endl;
         }
-    }
+    }*/
+	std::cout << n << " " << m;
     los.close();
 
     return 0;
